d16287c782498f0210856b0e8a6b3ccb931a22f6
Gabriel-Andrew Pollo-Guilbert <gabrielpolloguilbert@gmail.com>
2020-05-05 16:34:22 -0400

    Generate PKCS12 after certbot run

--- certbotw/main.go
+++ certbotw/main.go
@@ -4,12 +4,36 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"regexp"
 	"time"
 
 	"github.com/akamensky/argparse"
 )
 
-func generate_certificate(domain string, email string, dryrun bool) {
+func generate_pkcs12(domain string, dryrun bool) {
+	password := os.Getenv("PKCS12_PASSWORD")
+	if dryrun || password == "" {
+		fmt.Println("no PKCS12 certificate generated")
+		return
+	}
+
+	certs := "/etc/letsencrypt/live/" + domain
+	args := []string{"pkcs12", "-export"}
+	args = append(args, []string{"-out", certs + "/certificate.p12"}...)
+	args = append(args, []string{"-inkey", certs + "/privkey.pem"}...)
+	args = append(args, []string{"-in", certs + "/cert.pem"}...)
+	args = append(args, []string{"-certfile", certs + "/chain.pem"}...)
+	args = append(args, []string{"-password", "pass:" + password}...)
+
+	output, err := exec.Command("openssl", args...).CombinedOutput()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	fmt.Printf("%s", output)
+}
+
+func generate_certificate(domain string, email string, dryrun bool) bool {
 	args := []string{"certonly", "-n", "--standalone", "--agree-tos"}
 	args = append(args, []string{"--preferred-challenges", "http"}...)
 	args = append(args, []string{"--rsa-key-size", "4096"}...)
@@ -20,23 +44,37 @@ func generate_certificate(domain string, email string, dryrun bool) {
 		args = append(args, "--dry-run")
 	}
 
-	out, err := exec.Command("certbot", args...).CombinedOutput()
+	output, err := exec.Command("certbot", args...).CombinedOutput()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	fmt.Printf("%s", output)
+
+	matched, err := regexp.Match("not yet due for renewal", output)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
 
-	fmt.Printf("%s", out)
+	return !matched
 }
 
 func run(domains []string, email string, dryrun bool) {
 	for {
 		for _, domain := range domains {
 			fmt.Printf("\n=> generating for domain %s\n\n", domain)
-			generate_certificate(domain, email, dryrun)
+			renewed := generate_certificate(domain, email, dryrun)
+
+			if !renewed {
+				continue
+			}
+
+			fmt.Printf("\n=> generating PKCS12 for domain %s\n\n", domain)
+			generate_pkcs12(domain, dryrun)
 		}
 
-		fmt.Printf("\n=> Waiting a day\n\n")
+		fmt.Printf("\n=> waiting a day\n\n")
 		time.Sleep(24 * time.Hour)
 	}
 }
